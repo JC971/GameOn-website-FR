f825abb9576c125df1a2567f86f1a89058aab2dd
JC971 <99178903+JC971@users.noreply.github.com>
2023-03-07 13:32:28 +0100

    avancememt modifications formulaire

--- modal.js
+++ modal.js
@@ -19,20 +19,17 @@ const closeModalBtn = document.querySelector(".close");
 
 // Lancement de la modale on recupere le bouton de l'id je m'inscris pour pouvoir l'ouvrir
 
-modalBtn.addEventListener("click", launchModal );
+modalBtn.addEventListener("click", launchModal);
 
 // Fonction pour lancer la modale
 function launchModal() {
-  //on change le comportement de la modale qui etait en mode display none pour le passe en mode display block
+  //on change le comportement de la modale qui etait en mode display none pour le passer en mode display block
   modalbg.style.display = "block";
 }
 
-
-
-//je cible le bouton c'est parti dont la class est btn-submit
-
-let btn = document.querySelector(".btn-submit");
-console.log(btn)
+closeModale.addEventListener('click', function (e) {
+  modalbg.style.display = "none";
+})
 
 //je rentre les informations pour vérifier si c'est un prénom valide a l'aide de la fonction validateFirstNameInput
 
@@ -61,7 +58,6 @@ function validateLastNameInput(input) {
   }
 }
 
-
 var lastNameInput = document.getElementById("last");
 lastNameInput.addEventListener("input", function () {
   validateLastNameInput(lastNameInput);
@@ -72,41 +68,23 @@ var numberInput = document.getElementById("quantity");
 
 numberInput.addEventListener("input", function () {
   var inputValue = parseInt(numberInput.value);
-  if (isNaN(inputValue) || inputValue < 0 || inputValue > 10) {
+  if (isNaN(inputValue) || inputValue < 0 || inputValue > 30) {
     numberInput.setCustomValidity("Please enter a number between 0 and 10");
   } else {
     numberInput.setCustomValidity("");
   }
 });
 
-// checkbox
-//quels tournois
-var tournois = document.getElementById("location1");
-
 // les  conditions d'utilisation
 
 function validate() {
   if (!document.getElementById("checkbox1").checked) {
-    alert("Please accept the terms and conditions before submitting.");
+    alert("Acceptez les conditions d'utilisation avant de valider.");
     return false;
   }
   // If the checkbox is checked, submit the form
   return true;
 }
 
-//bouton radio choix 
 
-
-// close modal event avec icone croix
-
-closeModale.addEventListener('click', function (e) {
-  modalbg.style.display = "none";
-})
-// close modal
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-//hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-
-
+//
