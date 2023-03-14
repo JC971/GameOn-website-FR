51d5aff6a22c10caefd202f3164c32aa16c51e02
jean-claude <jean-claude.grech@orange.fr>
2023-03-14 11:57:43 +0100

    small nom

--- modal.js
+++ modal.js
@@ -1,11 +1,13 @@
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const partiBtn = document.querySelector("#parti");
+const prenom = document.querySelector("#first");
+const nom = document.querySelector("#last")
 const modalBtn = document.querySelector("#inscritBtn");
 const closeModale = document.querySelector("#closeModale");
 const formData = document.querySelectorAll(".formData");
-
 const closeModalBtn = document.querySelector(".close");
+
 
 function editNav() {
   var x = document.getElementById("myTopnav");
@@ -23,23 +25,96 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-closeModale.addEventListener('click', function (e) {
+//fonction pour fermer la modale
+function closeModal() {
   modalbg.style.display = "none";
-})
-//
-//small
+}
 
+
+//small
 
 //je rentre les informations pour vérifier si c'est un prénom valide a l'aide de la fonction validateFirstNameInput
 
-function validateFirstNameInput(input) {
-  var firstNameRegex = /^[a-zA-Z]+$/;
-  if (!firstNameRegex.test(input.value)) {
-    input.setCustomValidity("Prénom non valide");
-  } else {
-    input.setCustomValidity("");
+function validateFirstNameInput(firstname) {
+  console.log("validate first name", firstname.length);
+  if (firstname.length < 3)  {
+    console.log("entrer un prénom valide");
+    //faire apparaitre le messge d'erreur
+    document.getElementById("small_text").style.display = "block";
+    document.getElementById("first").style.border = "red 3px solid";
+    document.getElementById("first").style.color = "red ";
+  
+
+    // 
+  } else if (firstname.length > 2) {
+    document.getElementById("first").style.color = "green";
+    document.getElementById("first").style.border = "lightGreen 3px solid";
+    document.getElementById("small_text").style.display = "none";
+  }
+
+};
+
+
+
+
+//je rentre les informations pour vérifier si c'est un nom valide a l'aide de la fonction validateNameInput
+
+function validateLastNameInput(lastname) {
+  console.log('validate lastname', lastname.length);
+  if (lastname.length < 3) {
+    console.log("le nom n'est pas valide");
+    document.getElementById("small_nom").style.display = "block";
+    
   }
 }
+
+
+
+
+//fonction pour valider le nom de famille
+/*
+function validateNameInput(last) {
+  console.log("validate  name", .length);
+  if (firstname.length < 3)  {
+    console.log("entrer un prénom valide");
+    //faire apparaitre le messge d'erreur
+    document.getElementById("small_text").style.display = "block";
+    document.getElementById("first").style.border = "red 3px solid";
+    
+  
+
+    // 
+  } else if (firstname.length > 2) {
+    document.getElementById("first").style.color = "green";
+    document.getElementById("first").style.border = "lightGreen 3px solid";
+    document.getElementById("small_text").style.display = "none";
+  }
+
+}*/
+
+
+//je rentre les informations pour vérifier si c'est un nom valide a l'aide de la fonction validateNameInput
+/*
+function validateNameInput(name) {
+  console.log("validate Name", nom.length);
+
+  if (name.length < 3) {
+    console.log("message d'erreur"){
+      //faire apparaitre un message d'erreur
+    } else {
+      //effacer me message
+    }
+  }
+
+  
+}
+
+*/
+
+
+
+
+
 //je selectionne les donnees du champ avec son id first
 var firstNameInput = document.getElementById("first");
 firstNameInput.addEventListener("input", function () {
@@ -47,7 +122,7 @@ firstNameInput.addEventListener("input", function () {
 });
 
 // je rentre les informations pour vérifier le Nom
-
+/*
 function validateLastNameInput(input) {
   var lastNameRegex = /^[a-zA-Z]+$/;
   if (!lastNameRegex.test(input.value)) {
@@ -60,7 +135,7 @@ function validateLastNameInput(input) {
 var lastNameInput = document.getElementById("last");
 lastNameInput.addEventListener("input", function () {
   validateLastNameInput(lastNameInput);
-});
+});*/
 
 // nombre de tournois 
 var numberInput = document.getElementById("quantity");
@@ -85,17 +160,36 @@ function validate() {
   return true;
 }
 
+
+
+
 // Lancement de la modale on recupere le bouton de l'id je m'inscris pour pouvoir l'ouvrir
 
 modalBtn.addEventListener("click", launchModal);
 
-/*
-surname.addEventListener('input', checkFormCompletion);
-lastName.addEventListener('input', checkFormCompletion);
-email.addEventListener('input', checkFormCompletion);
-birth.addEventListener('input', checkFormCompletion);
-quantity.addEventListener('input', checkFormCompletion);
+//fermeture de la modale 
+closeModale.addEventListener('click', closeModal);
 
+// parti 
+partiBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  var textPrenom = prenom.value;
+  validateFirstNameInput(textPrenom);
+  //
+  var textNom = nom.value;
+  validateLastNameInput(textNom);
+
+});
+/*
+partiBtn.addEventListener('click', function (event) {
+  
+  var textNom = nom.value;
+  validateLastNameInput(textNom);
+})*/
+
+
+
+/*
 function checkFormCompletion() {
   if (surname.value && lastName.value && email.value && birth.value && quantity.value) {
     alert('merci');
