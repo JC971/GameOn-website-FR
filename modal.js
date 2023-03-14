82a1773caee663c93d729e418841dc08e48b0fff
jean-claude <jean-claude.grech@orange.fr>
2023-03-14 17:42:30 +0100

    bouton inscris media

--- modal.js
+++ modal.js
@@ -30,9 +30,6 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-
-//small
-
 //je rentre les informations pour vérifier si c'est un prénom valide a l'aide de la fonction validateFirstNameInput
 
 function validateFirstNameInput(firstname) {
@@ -44,7 +41,6 @@ function validateFirstNameInput(firstname) {
     document.getElementById("first").style.border = "red 3px solid";
     document.getElementById("first").style.color = "red ";
   
-
     // 
   } else if (firstname.length > 2) {
     document.getElementById("first").style.color = "green ";
@@ -53,7 +49,6 @@ function validateFirstNameInput(firstname) {
   }
 
 };
-
 
 //je rentre les informations pour vérifier si c'est un nom valide a l'aide de la fonction validateNameInput
 
@@ -73,74 +68,12 @@ function validateLastNameInput(lastname) {
   }
 };
 
-
-
-
-//fonction pour valider le nom de famille
-/*
-function validateNameInput(last) {
-  console.log("validate  name", .length);
-  if (firstname.length < 3)  {
-    console.log("entrer un prénom valide");
-    //faire apparaitre le messge d'erreur
-    document.getElementById("small_text").style.display = "block";
-    document.getElementById("first").style.border = "red 3px solid";
-    
-  
-
-    // 
-  } else if (firstname.length > 2) {
-    document.getElementById("first").style.color = "green";
-    document.getElementById("first").style.border = "lightGreen 3px solid";
-    document.getElementById("small_text").style.display = "none";
-  }
-
-}*/
-
-
-//je rentre les informations pour vérifier si c'est un nom valide a l'aide de la fonction validateNameInput
-/*
-function validateNameInput(name) {
-  console.log("validate Name", nom.length);
-
-  if (name.length < 3) {
-    console.log("message d'erreur"){
-      //faire apparaitre un message d'erreur
-    } else {
-      //effacer me message
-    }
-  }
-
-  
-}
-
-*/
-
-
-
-
-
 //je selectionne les donnees du champ avec son id first
 var firstNameInput = document.getElementById("first");
 firstNameInput.addEventListener("input", function () {
   validateFirstNameInput(firstNameInput);
 });
 
-// je rentre les informations pour vérifier le Nom
-/*
-function validateLastNameInput(input) {
-  var lastNameRegex = /^[a-zA-Z]+$/;
-  if (!lastNameRegex.test(input.value)) {
-    input.setCustomValidity("Le Nom de famille n'est pas valide");
-  } else {
-    input.setCustomValidity("");
-  }
-}
-
-var lastNameInput = document.getElementById("last");
-lastNameInput.addEventListener("input", function () {
-  validateLastNameInput(lastNameInput);
-});*/
 
 // nombre de tournois 
 var numberInput = document.getElementById("quantity");
@@ -165,9 +98,6 @@ function validate() {
   return true;
 }
 
-
-
-
 // Lancement de la modale on recupere le bouton de l'id je m'inscris pour pouvoir l'ouvrir
 
 modalBtn.addEventListener("click", launchModal);
@@ -185,20 +115,4 @@ partiBtn.addEventListener('click', function (event) {
   validateLastNameInput(textNom);
 
 });
-/*
-partiBtn.addEventListener('click', function (event) {
-  
-  var textNom = nom.value;
-  validateLastNameInput(textNom);
-})*/
 
-
-
-/*
-function checkFormCompletion() {
-  if (surname.value && lastName.value && email.value && birth.value && quantity.value) {
-    alert('merci');
-    form.reset();
-    form.style.display = 'none'
-  }
-}*/
